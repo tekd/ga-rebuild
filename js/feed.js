33047f6c8801de82620986b1ad098f487a5ebd42
Denny Tek <tek.denny@gmail.com>
2016-04-07 17:25:30 -0400

    Clean up html and css on base and index

--- js/feed.js
+++ js/feed.js
@@ -13,7 +13,7 @@ function parseXml(xml) {
   console.log(xml);
   var feedArray = $(xml).find("item");
     // APPENDING 1 FOR NOW
-  var topFourItems = feedArray.splice(0,1);
+  var topFourItems = feedArray.splice(0,4);
   // debugger
   var digestHtml = [];
   topFourItems.forEach(function(item){;
@@ -24,5 +24,5 @@ function parseXml(xml) {
     $("#slider-div").append('<section id="slide-id" class="b-slide m-purple"><header><h4>The GovLab Digest</h4></header><div class="row e-content"><div class="column medium-6"><h2 class="e-project-name">' + title + '</h2><h3><strong>By: </strong>'+ author +'</h3></div><div class="column medium-6"><p>' + description + '</p></div></div><div class="row e-content"><div class="column medium-6"><div class="e-action"><a class="b-button m-naked" href="http://thegovlab.org/category/govlab-digest/">Read the Digest<i class="material-icons">arrow_forward</i></a></div></div><div class="column medium-6"><div class="e-action"><a class="b-button m-naked" href=\"'+link +'\">Find out more<i class="material-icons">arrow_forward</i></a></div></div></div></section>');
   }); 
 
-
 }
+
